6002963b685aa0b696633a016c0219673098581a
Wilson Yan <wilson1.yan@berkeley.edu>
2019-08-31 19:33:07 -0700

    Small fix

--- dm_control/suite/cloth_v7.py
+++ dm_control/suite/cloth_v7.py
@@ -63,7 +63,7 @@ class Cloth(base.Task):
   """A point_mass `Task` to reach target with smooth reward."""
 
   def __init__(self, randomize_gains, random=None, pixel_size=64, camera_id=0,
-               reward='diagonal', distance_weight=0.0):
+               reward='diagonal', distance_weight=0.0, eval=False):
     """Initialize an instance of `PointMass`.
 
     Args:
