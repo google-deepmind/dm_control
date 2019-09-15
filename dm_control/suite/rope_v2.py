59be9f6fb8059dc10ffb5133fc02c3932233517f
Wilson Yan <wilson1.yan@berkeley.edu>
2019-09-15 13:25:04 -0700

    wip update

--- dm_control/suite/rope_v2.py
+++ dm_control/suite/rope_v2.py
@@ -20,23 +20,17 @@ from __future__ import division
 from __future__ import print_function
 
 import collections
-from dm_control.rl import specs
+from dm_env import specs
 from dm_control import mujoco
 from dm_control.rl import control
 from dm_control.suite import base
 from dm_control.suite import common
-from dm_control.suite.utils import randomizers
 from dm_control.utils import containers
-from dm_control.utils import rewards
 import numpy as np
-import random
-import mujoco_py
-import os
+
 import math
-from PIL import Image,ImageColor
-from scipy.stats import linregress
-from dm_control.suite.wrappers.modder import LightModder
-from imageio import imsave
+
+
 _DEFAULT_TIME_LIMIT = 20
 SUITE = containers.TaggedTasks()
 
@@ -70,7 +64,7 @@ class Physics(mujoco.Physics):
 class Rope(base.Task):
   """A point_mass `Task` to reach target with smooth reward."""
 
-  def __init__(self, randomize_gains, random=None):
+  def __init__(self, randomize_gains, random=None, random_location=True):
     """Initialize an instance of `PointMass`.
 
     Args:
@@ -80,15 +74,18 @@ class Rope(base.Task):
         automatically (default).
     """
     self._randomize_gains = randomize_gains
-    # self.action_spec=specs.BoundedArraySpec(
-    # shape=(2,), dtype=np.float, minimum=0.0, maximum=1.0)
+    self._random_location = random_location
     super(Rope, self).__init__(random=random)
 
   def action_spec(self, physics):
     """Returns a `BoundedArraySpec` matching the `physics` actuators."""
-
-    return specs.BoundedArraySpec(
-        shape=(2,), dtype=np.float, minimum=[-1.0] * 2, maximum=[1.0] * 2)
+    if self._random_location:
+      return specs.BoundedArray(
+          shape=(2,), dtype=np.float, minimum=[-1.0] * 2, maximum=[1.0] * 2)
+    else:
+      return specs.BoundedArray(
+          shape=(3,), dtype=np.float, minimum=[-1.0] * 3, maximum=[1.0] * 3
+      )
 
   def initialize_episode(self,physics):
 
@@ -96,20 +93,20 @@ class Rope(base.Task):
     # physics.named.data.xfrc_applied['B10', :3] = np.array([0, 0, -2])
     # physics.named.data.xfrc_applied['B0_8', :3] = np.array([0.2,0.2,0.1])
 
-    self.dof_damping = np.concatenate([np.zeros((6)), np.ones((48)) * 0.002], axis=0)
-    self.body_mass = np.concatenate([np.zeros(1), np.ones(25) * 0.00563])
-    self.body_inertia = np.concatenate([np.zeros((1, 3)), np.tile(np.array([[4.58e-07,  4.58e-07,  1.8e-07]]), (25, 1))],
-                                       axis=0)
-    self.geom_friction = np.tile(np.array([[1, 0.005, 0.001]]), (26, 1))
-    self.cam_pos = np.array([0, 0, 0.75])
-    self.cam_quat = np.array([1, 0, 0, 0])
-
-    self.light_diffuse = np.array([0, 0, 0])
-    self.light_specular = np.array([0, 0, 0])
-    self.light_ambient = np.array([0, 0, 0])
-    self.light_castshadow = np.array([1])
-    self.light_dir = np.array([0, 0, -1])
-    self.light_pos = np.array([0, 0, 1])
+    # self.dof_damping = np.concatenate([np.zeros((6)), np.ones((48)) * 0.002], axis=0)
+    # self.body_mass = np.concatenate([np.zeros(1), np.ones(25) * 0.00563])
+    # self.body_inertia = np.concatenate([np.zeros((1, 3)), np.tile(np.array([[4.58e-07,  4.58e-07,  1.8e-07]]), (25, 1))],
+    #                                    axis=0)
+    # self.geom_friction = np.tile(np.array([[1, 0.005, 0.001]]), (26, 1))
+    # self.cam_pos = np.array([0, 0, 0.75])
+    # self.cam_quat = np.array([1, 0, 0, 0])
+    #
+    # self.light_diffuse = np.array([0, 0, 0])
+    # self.light_specular = np.array([0, 0, 0])
+    # self.light_ambient = np.array([0, 0, 0])
+    # self.light_castshadow = np.array([1])
+    # self.light_dir = np.array([0, 0, -1])
+    # self.light_pos = np.array([0, 0, 1])
 
 
 
@@ -118,13 +115,7 @@ class Rope(base.Task):
     render_kwargs['width'] = W
     render_kwargs['height'] = W
     image = physics.render(**render_kwargs)
-   # image_dir = os.path.expanduser('~/softlearning')
-   # image_path = os.path.join(image_dir,f'rope_v1.png')
-   # imsave(image_path,image)
     self.image = image
-#    image_dim = image[:, :, 1].reshape((W, W, 1))
- #   self.mask = np.all(image_dim > 180, axis=2).astype(int)
-#    physics.named.data.qfrc_applied[:2]=np.random.uniform(-0.6,0.6,size=2)
 
     physics.named.data.xfrc_applied[CORNER_INDEX_ACTION, :2] = np.random.uniform(-0.8, 0.8, size=8).reshape((4,2))
     super(Rope, self).initialize_episode(physics)
@@ -134,156 +125,35 @@ class Rope(base.Task):
     physics.named.data.xfrc_applied[:,:3]=np.zeros((3,))
     physics.named.data.qfrc_applied[:2]=0
     goal_position = action[:2]
-    # location =action[3:].astype(int)
 
-    print('original action', goal_position)
+    if self._random_location:
+      assert len(action) == 2
+      goal_position = action
+    else:
+      assert len(action) == 3
+      goal_position = action[:2]
+      location = action[2]
 
     goal_position = goal_position * 0.05
-    print('scaled_action', goal_position)
     location = self.current_loc
     if location is None:
       return
-    print('location', location)
-    
-    # visual randomization
-    # light randomization
-    lightmodder = LightModder(physics)
-    # ambient_value=lightmodder.get_ambient('light')
-    ambient_value = self.light_ambient.copy() + np.random.uniform(-0.5, 0.5, size=3)
-    # ambient_value=np.array([.2, .2 ,.2])
-    lightmodder.set_ambient('light', ambient_value)
 
-    # shadow_value=lightmodder.get_castshadow('light')
-    shadow_value = self.light_castshadow.copy()
-    lightmodder.set_castshadow('light', shadow_value + np.random.uniform(0, 40))
-    # diffuse_value=lightmodder.get_diffuse('light')
-    diffuse_value = self.light_diffuse.copy()
-    # lightmodder.set_diffuse('light',diffuse_value+np.random.uniform(-0.1,0.1,))
-    lightmodder.set_diffuse('light', np.array([0, 0, 0]))
+    corner_action = index
+    corner_geom = index
 
-    # dir_value=lightmodder.get_dir('light')
-    dir_value = self.light_dir.copy()
-    lightmodder.set_dir('light', dir_value + np.random.uniform(-0.1, 0.1))
-    # pos_value=lightmodder.get_pos('light')
-    pos_value = self.light_pos.copy()
-    lightmodder.set_pos('light', pos_value + np.random.uniform(-0.1, 0.1))
-    # specular_value=lightmodder.get_specular('light')
-    specular_value = self.light_specular.copy()
-    lightmodder.set_specular('light', specular_value + np.random.uniform(-0.1, 0.1))
+    position = goal_position + physics.named.data.geom_xpos[corner_geom,:2]
+    dist = position - physics.named.data.geom_xpos[corner_geom,:2]
 
-    # physics randomization
-
-    # damping randomization
-
-    physics.named.model.dof_damping[:] = np.random.uniform(0, 0.0001) + self.dof_damping
-
-    # # friction randomization
-    geom_friction = self.geom_friction.copy()
-    physics.named.model.geom_friction[1:, 0] = np.random.uniform(-0.5, 0.5) + geom_friction[1:, 0]
-    #
-    physics.named.model.geom_friction[1:, 1] = np.random.uniform(-0.002, 0.002) + geom_friction[1:, 1]
-    #
-    physics.named.model.geom_friction[1:, 2] = np.random.uniform(-0.0005, 0.0005) + geom_friction[1:, 2]
-    #
-    # # inertia randomization
-    body_inertia = self.body_inertia.copy()
-    physics.named.model.body_inertia[1:] = np.random.uniform(-0.5, 0.5) * 1e-07 + body_inertia[1:]
-    #
-    # mass randomization
-    body_mass = self.body_mass.copy()
-
-    physics.named.model.body_mass[1:] = np.random.uniform(-0.0005, 0.0005) + body_mass[1:]
-
-
-    # corner_geom = 'G0'
-    # corner_action = 'B0'
-    # position = goal_position + physics.named.data.geom_xpos[corner_geom,:2]
-    # dist = position - physics.named.data.geom_xpos[corner_geom,:2]
-    #
-    # loop = 0
-    # while np.linalg.norm(dist) > 0.025:
-    #   loop += 1
-    #   if loop > 40:
-    #     break
-    #   physics.named.data.xfrc_applied[corner_action, :2] = dist * 10
-    #   physics.step()
-    #   self.after_step(physics)
-    #   dist = position - physics.named.data.geom_xpos[corner_geom,:2]
-    # print('loop', loop)
-    #
-    # computing the mapping from geom_xpos to location in image
-    cam_fovy = physics.named.model.cam_fovy['fixed']
-    f = 0.5 * W / math.tan(cam_fovy * math.pi / 360)
-    cam_matrix = np.array([[f, 0, W / 2], [0, f, W / 2], [0, 0, 1]])
-    cam_mat = physics.named.data.cam_xmat['fixed'].reshape((3, 3))
-    cam_pos = physics.named.data.cam_xpos['fixed'].reshape((3, 1))
-    cam = np.concatenate([cam_mat, cam_pos], axis=1)
-    cam_pos_all = np.zeros((26, 3, 1))
-    for i in range(26):
-      geom_xpos_added = np.concatenate([physics.data.geom_xpos[i], np.array([1])]).reshape((4, 1))
-      cam_pos_all[i] = cam_matrix.dot(cam.dot(geom_xpos_added)[:3])
-
-    # cam_pos_xy=cam_pos_all[5:,:]
-    cam_pos_xy = np.rint(cam_pos_all[:, :2].reshape((26, 2)) / cam_pos_all[:, 2])
-    cam_pos_xy = cam_pos_xy.astype(int)
-    cam_pos_xy[:, 1] = W - cam_pos_xy[:, 1]
-
-    epsilon = 4
-    possible_index = []
-    possible_z = []
-    for i in range(26):
-      # flipping the x and y to make sure it corresponds to the real location
-      if abs(cam_pos_xy[i][0] - location[0, 1]) < epsilon and abs(
-              cam_pos_xy[i][1] - location[0, 0]) < epsilon and i > 0 and np.all(cam_pos_xy[i] < W) and np.all(
-        cam_pos_xy[i] >= 0):
-        possible_index.append(i)
-        possible_z.append(physics.data.geom_xpos[i, 2])
-
-    # im = Image.new('RGBA', (W, W), ImageColor.getcolor('grey', 'RGBA'))
-    # location_range = self.location_range
-    # for i in range(location_range.shape[0]):
-    #   im.putpixel((location_range[i, 1], location_range[i, 0]), ImageColor.getcolor('blue', 'RGBA'))
-    # # im_array=np.zeros((64,64))
-    # for i in range(30):
-    #   # print(cam_pos[i])
-    #   if i >= 5 and np.all(cam_pos_xy[i] >= 0) and np.all(cam_pos_xy[i] < W):
-    #     print(cam_pos_xy[i])
-    #     im.putpixel((cam_pos_xy[i][0], cam_pos_xy[i][1]), ImageColor.getcolor('green', 'RGBA'))
-    #
-    # for i in range(len(possible_index)):
-    #   im.putpixel((cam_pos_xy[possible_index][i][0], cam_pos_xy[possible_index][i][1]),
-    #               ImageColor.getcolor('red', 'RGBA'))
-    # print(possible_index)
-    # if possible_index != []:
-    #   index = possible_index[possible_z.index(max(possible_z))]
-    #   im.putpixel((cam_pos_xy[index][0], cam_pos_xy[index][1]), ImageColor.getcolor('pink', 'RGBA'))
-    # im.putpixel((int(location[0, 1]), int(location[0, 0])), ImageColor.getcolor('yellow', 'RGBA'))
-    # image_save_dir = os.path.expanduser('~/softlearning/cloth_resources/image_camera')
-    # image_save_path = os.path.join(image_save_dir, f'cloth_point_{location[0, 0]}.png')
-    # image_save_path_1 = os.path.join(image_save_dir, f'cloth_{location[0, 0]}.png')
-    # im.save(image_save_path)
-    # image = physics.render(camera_id=0, width=W, height=W)
-    # imsave(image_save_path_1, image)
-
-    if possible_index != []:
-      index = possible_index[possible_z.index(max(possible_z))]
-
-      corner_action = index
-      corner_geom = index
-
-      position = goal_position + physics.named.data.geom_xpos[corner_geom,:2]
+    loop = 0
+    while np.linalg.norm(dist) > 0.025:
+      loop += 1
+      if loop > 40:
+        break
+      physics.named.data.xfrc_applied[corner_action, :2] = dist * 20
+      physics.step()
+      self.after_step(physics)
       dist = position - physics.named.data.geom_xpos[corner_geom,:2]
-
-      loop = 0
-      while np.linalg.norm(dist) > 0.025:
-        loop += 1
-        if loop > 40:
-          break
-        physics.named.data.xfrc_applied[corner_action, :2] = dist * 20
-        physics.step()
-        self.after_step(physics)
-        dist = position - physics.named.data.geom_xpos[corner_geom,:2]
-      print('loop', loop)
 
   def get_termination(self,physics):
     if self.num_loc<1:
