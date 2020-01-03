002de80da47a5e706ce5e7876299533189377ff5
DeepMind <noreply@google.com>
2020-01-03 02:42:59 -0800

    Added observations appendages_pos and body_quats, which measure the appendage positions and body quaternions in the egocentric frame.
    PiperOrigin-RevId: 287965806
    Change-Id: I79ec2278eec6302fd5e441950d1ebdfb1453ce21

--- setup.py
+++ setup.py
@@ -166,7 +166,7 @@ def find_data_files(package_dir, patterns):
 
 setup(
     name='dm_control',
-    version='0.0.287770839',
+    version='0.0.287965806',
     description='Continuous control environments and MuJoCo Python bindings.',
     author='DeepMind',
     license='Apache License, Version 2.0',
