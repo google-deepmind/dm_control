9d30a8dce1cce32d53ebf442566577879e6756d9
Wilson Yan <wilson1.yan@berkeley.edu>
2020-01-12 20:20:33 -0800

    cloth colored working

--- dm_control/suite/__init__.py
+++ dm_control/suite/__init__.py
@@ -60,6 +60,7 @@ from dm_control.suite import rope_v2
 from dm_control.suite import rope_sac
 from dm_control.suite import rope_two_hand
 from dm_control.suite import rope_colored
+from dm_control.suite import cloth_colored
 
 # Find all domains imported.
 _DOMAINS = {name: module for name, module in locals().items()
