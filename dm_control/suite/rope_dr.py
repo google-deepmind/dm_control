c34ce8d690169ffc9271f9c4d6b6905b57b3c2cf
Wilson Yan <wilson1.yan@berkeley.edu>
2020-02-17 18:51:55 -0800

    dr option, default False

--- dm_control/suite/rope_dr.py
+++ dm_control/suite/rope_dr.py
@@ -72,7 +72,8 @@ class Physics(mujoco.Physics):
 class Rope(base.Task):
     """A point_mass `Task` to reach target with smooth reward."""
 
-    def __init__(self, randomize_gains, random=None, random_pick=False, init_flat=True):
+    def __init__(self, randomize_gains, random=None, random_pick=False, init_flat=True,
+                 use_dr=False):
         """Initialize an instance of `PointMass`.
 
         Args:
@@ -85,6 +86,7 @@ class Rope(base.Task):
         self._init_flat = init_flat
         self._random_pick = random_pick
         self._n_geoms = 21
+        self._use_dr = use_dr
         super(Rope, self).__init__(random=random)
 
     def action_spec(self, physics):
@@ -101,21 +103,22 @@ class Rope(base.Task):
         return np.array(geoms)
 
     def initialize_episode(self, physics):
-        self.dof_damping = np.concatenate([np.zeros((6)), np.ones(2 * (self._n_geoms - 1)) * 0.002], axis=0)
-        self.body_mass = np.concatenate([np.zeros(1), np.ones(self._n_geoms) * 0.00563])
-        self.body_inertia = np.concatenate(
-            [np.zeros((1, 3)), np.tile(np.array([[4.58e-07, 4.58e-07, 1.8e-07]]), (self._n_geoms, 1))],
-            axis=0)
-        self.geom_friction = np.tile(np.array([[1, 0.005, 0.001]]), (self._n_geoms + 5, 1))
-        self.cam_pos = np.array([0, 0, 0.75])
-        self.cam_quat = np.array([1, 0, 0, 0])
+        if self._use_dr:
+            self.dof_damping = np.concatenate([np.zeros((6)), np.ones(2 * (self._n_geoms - 1)) * 0.002], axis=0)
+            self.body_mass = np.concatenate([np.zeros(1), np.ones(self._n_geoms) * 0.00563])
+            self.body_inertia = np.concatenate(
+                [np.zeros((1, 3)), np.tile(np.array([[4.58e-07, 4.58e-07, 1.8e-07]]), (self._n_geoms, 1))],
+                axis=0)
+            self.geom_friction = np.tile(np.array([[1, 0.005, 0.001]]), (self._n_geoms + 5, 1))
+            self.cam_pos = np.array([0, 0, 0.75])
+            self.cam_quat = np.array([1, 0, 0, 0])
 
-        self.light_diffuse = np.array([0, 0, 0])
-        self.light_specular = np.array([0, 0, 0])
-        self.light_ambient = np.array([0, 0, 0])
-        self.light_castshadow = np.array([1])
-        self.light_dir = np.array([0, 0, -1])
-        self.light_pos = np.array([0, 0, 1])
+            self.light_diffuse = np.array([0, 0, 0])
+            self.light_specular = np.array([0, 0, 0])
+            self.light_ambient = np.array([0, 0, 0])
+            self.light_castshadow = np.array([1])
+            self.light_dir = np.array([0, 0, -1])
+            self.light_pos = np.array([0, 0, 1])
 
         render_kwargs = {}
         render_kwargs['camera_id'] = 0
@@ -141,47 +144,48 @@ class Rope(base.Task):
             goal_position = goal_position * 0.075
             location = self.current_loc
 
-        # visual randomization
-        # light randomization
-        lightmodder = LightModder(physics)
-        ambient_value = self.light_ambient.copy() + np.random.uniform(-0.5, 0.5, size=3)
-        lightmodder.set_ambient('light', ambient_value)
+        if self._use_dr:
+            # visual randomization
+            # light randomization
+            lightmodder = LightModder(physics)
+            ambient_value = self.light_ambient.copy() + np.random.uniform(-0.5, 0.5, size=3)
+            lightmodder.set_ambient('light', ambient_value)
 
-        shadow_value = self.light_castshadow.copy()
-        lightmodder.set_castshadow('light', shadow_value + np.random.uniform(0, 40))
-        diffuse_value = self.light_diffuse.copy()
-        lightmodder.set_diffuse('light',diffuse_value+np.random.uniform(-0.1,0.1,))
-        lightmodder.set_diffuse('light', np.array([0, 0, 0]))
+            shadow_value = self.light_castshadow.copy()
+            lightmodder.set_castshadow('light', shadow_value + np.random.uniform(0, 40))
+            diffuse_value = self.light_diffuse.copy()
+            lightmodder.set_diffuse('light',diffuse_value+np.random.uniform(-0.1,0.1,))
+            lightmodder.set_diffuse('light', np.array([0, 0, 0]))
 
-        dir_value = self.light_dir.copy()
-        lightmodder.set_dir('light', dir_value + np.random.uniform(-0.1, 0.1))
-        pos_value = self.light_pos.copy()
-        lightmodder.set_pos('light', pos_value + np.random.uniform(-0.1, 0.1))
-        specular_value = self.light_specular.copy()
-        lightmodder.set_specular('light', specular_value + np.random.uniform(-0.1, 0.1))
+            dir_value = self.light_dir.copy()
+            lightmodder.set_dir('light', dir_value + np.random.uniform(-0.1, 0.1))
+            pos_value = self.light_pos.copy()
+            lightmodder.set_pos('light', pos_value + np.random.uniform(-0.1, 0.1))
+            specular_value = self.light_specular.copy()
+            lightmodder.set_specular('light', specular_value + np.random.uniform(-0.1, 0.1))
 
-        # physics randomization
+            # physics randomization
 
-        # damping randomization
+            # damping randomization
 
-        physics.named.model.dof_damping[:] = np.random.uniform(0, 0.0001) + self.dof_damping
+            physics.named.model.dof_damping[:] = np.random.uniform(0, 0.0001) + self.dof_damping
 
-        # # friction randomization
-        geom_friction = self.geom_friction.copy()
-        physics.named.model.geom_friction[1:, 0] = np.random.uniform(-0.5, 0.5) + geom_friction[1:, 0]
+            # # friction randomization
+            geom_friction = self.geom_friction.copy()
+            physics.named.model.geom_friction[1:, 0] = np.random.uniform(-0.5, 0.5) + geom_friction[1:, 0]
 
-        physics.named.model.geom_friction[1:, 1] = np.random.uniform(-0.002, 0.002) + geom_friction[1:, 1]
+            physics.named.model.geom_friction[1:, 1] = np.random.uniform(-0.002, 0.002) + geom_friction[1:, 1]
 
-        physics.named.model.geom_friction[1:, 2] = np.random.uniform(-0.0005, 0.0005) + geom_friction[1:, 2]
+            physics.named.model.geom_friction[1:, 2] = np.random.uniform(-0.0005, 0.0005) + geom_friction[1:, 2]
 
-        # # inertia randomization
-        body_inertia = self.body_inertia.copy()
-        physics.named.model.body_inertia[1:] = np.random.uniform(-0.5, 0.5) * 1e-07 + body_inertia[1:]
+            # # inertia randomization
+            body_inertia = self.body_inertia.copy()
+            physics.named.model.body_inertia[1:] = np.random.uniform(-0.5, 0.5) * 1e-07 + body_inertia[1:]
 
-        # mass randomization
-        body_mass = self.body_mass.copy()
+            # mass randomization
+            body_mass = self.body_mass.copy()
 
-        physics.named.model.body_mass[1:] = np.random.uniform(-0.0005, 0.0005) + body_mass[1:]
+            physics.named.model.body_mass[1:] = np.random.uniform(-0.0005, 0.0005) + body_mass[1:]
 
         # computing the mapping from geom_xpos to location in image
         cam_fovy = physics.named.model.cam_fovy['fixed']
