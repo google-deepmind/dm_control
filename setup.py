d677e2b57f20a85211e4cedd4b02012b254ab023
Alistair Muldal <alimuldal@google.com>
2020-03-13 09:35:47 -0700

    Update stable versions in `requirements.txt`
    PiperOrigin-RevId: 300771433
    Change-Id: I4a62b10474781234b4943536968a794f74c6c9a2

--- setup.py
+++ setup.py
@@ -166,7 +166,7 @@ def find_data_files(package_dir, patterns):
 
 setup(
     name='dm_control',
-    version='0.0.300648743',
+    version='0.0.300771433',
     description='Continuous control environments and MuJoCo Python bindings.',
     author='DeepMind',
     license='Apache License, Version 2.0',
