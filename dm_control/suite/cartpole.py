2bb45ecf3345b411555e9bfa0be4c68f86d275b5
DeepMind <noreply@google.com>
2020-05-14 02:53:01 -0700

    Typo fix.
    PiperOrigin-RevId: 311497959
    Change-Id: I553b4bf37007a43f7b32c94022dd21275e6006ec

--- dm_control/suite/cartpole.py
+++ dm_control/suite/cartpole.py
@@ -77,7 +77,7 @@ def swingup(time_limit=_DEFAULT_TIME_LIMIT, random=None,
 @SUITE.add('benchmarking')
 def swingup_sparse(time_limit=_DEFAULT_TIME_LIMIT, random=None,
                    environment_kwargs=None):
-  """Returns the sparse reward variant of teh Cartpole Swing-Up task."""
+  """Returns the sparse reward variant of the Cartpole Swing-Up task."""
   physics = Physics.from_xml_string(*get_model_and_assets())
   task = Balance(swing_up=True, sparse=True, random=random)
   environment_kwargs = environment_kwargs or {}
--- setup.py
+++ setup.py
@@ -166,7 +166,7 @@ def find_data_files(package_dir, patterns):
 
 setup(
     name='dm_control',
-    version='0.0.310965000',
+    version='0.0.311497959',
     description='Continuous control environments and MuJoCo Python bindings.',
     author='DeepMind',
     license='Apache License, Version 2.0',
