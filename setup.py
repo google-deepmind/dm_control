6df3c412413ae5cb26e66db01169d081f79817b6
Saran Tunyasuvunakool <stunya@google.com>
2020-07-10 13:02:26 -0700

    Internal change.
    PiperOrigin-RevId: 320659787
    Change-Id: I513c9dbc04c9cdc30dde5806bd8a092156e7a45c

--- setup.py
+++ setup.py
@@ -177,7 +177,7 @@ def find_data_files(package_dir, patterns, excludes=()):
 
 setup(
     name='dm_control',
-    version='0.0.319497192',
+    version='0.0.320659787',
     description='Continuous control environments and MuJoCo Python bindings.',
     author='DeepMind',
     license='Apache License, Version 2.0',
