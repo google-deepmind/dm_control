8948181b039a89e3c43dad2ceed312574e2d53df
Saran Tunyasuvunakool <stunya@google.com>
2020-07-17 11:37:33 -0700

    [STAGING] Add CMU mocap data loading code to dm_control.
    PiperOrigin-RevId: 321821070
    Change-Id: Ib493bdb43636b642c24462ee87885e2e0faa8c04

--- setup.py
+++ setup.py
@@ -177,7 +177,7 @@ def find_data_files(package_dir, patterns, excludes=()):
 
 setup(
     name='dm_control',
-    version='0.0.321795972',
+    version='0.0.321821070',
     description='Continuous control environments and MuJoCo Python bindings.',
     author='DeepMind',
     license='Apache License, Version 2.0',
