9e0fe0f0f9713a2a993ca78776529011d6c5fbeb
Saran Tunyasuvunakool <stunya@google.com>
2020-08-20 19:39:59 +0100

    Document support for Python 3.8.
    Pin dependency versions in requirements.txt to ones for which Python 3.8 wheels are available on PyPI.
    
    Pin protobuf version in setup.py to be no less than the one that was used to generate the precompiled Python source.
    
    PiperOrigin-RevId: 327666647
    Change-Id: If90d5c68afdba5c12aa29155187358a1cea817ac

--- setup.py
+++ setup.py
@@ -177,7 +177,7 @@ def find_data_files(package_dir, patterns, excludes=()):
 
 setup(
     name='dm_control',
-    version='0.0.327608358',
+    version='0.0.327666647',
     description='Continuous control environments and MuJoCo Python bindings.',
     author='DeepMind',
     license='Apache License, Version 2.0',
@@ -193,7 +193,7 @@ setup(
         'labmaze',
         'lxml',
         'numpy',
-        'protobuf',
+        'protobuf >= 3.12.2',
         'pyopengl >= 3.1.4',
         'pyparsing',
         'requests',
