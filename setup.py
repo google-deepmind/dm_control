e0f115de1cf180bea87357ffa6b17f6d3de2684e
DeepMind <noreply@google.com>
2020-10-07 10:06:04 -0700

    Internal change to mocap proto definition.
    PiperOrigin-RevId: 335893588
    Change-Id: Ica6d4b6b3369c090dabdac7c5a8028f547f7a1ee

--- setup.py
+++ setup.py
@@ -177,7 +177,7 @@ def find_data_files(package_dir, patterns, excludes=()):
 
 setup(
     name='dm_control',
-    version='0.0.335603005',
+    version='0.0.335893588',
     description='Continuous control environments and MuJoCo Python bindings.',
     author='DeepMind',
     license='Apache License, Version 2.0',
