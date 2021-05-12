cd340e711e727897ceed5bc7f0b3454af7050980
Siqi Liu <liusiqi@google.com>
2021-05-12 08:06:14 -0700

    Introduce factory method with the option to use humanoids.
    PiperOrigin-RevId: 373371306
    Change-Id: I51b0a72afdce6a3891d608b31d3ecfb387750250

--- dm_control/locomotion/soccer/__init__.py
+++ dm_control/locomotion/soccer/__init__.py
@@ -27,6 +27,9 @@ from dm_control.locomotion.soccer.observables import CoreObservablesAdder
 from dm_control.locomotion.soccer.observables import InterceptionObservablesAdder
 from dm_control.locomotion.soccer.observables import MultiObservablesAdder
 from dm_control.locomotion.soccer.observables import ObservablesAdder
+from dm_control.locomotion.soccer.pitch import MINI_FOOTBALL_GOAL_SIZE
+from dm_control.locomotion.soccer.pitch import MINI_FOOTBALL_MAX_AREA_PER_HUMANOID
+from dm_control.locomotion.soccer.pitch import MINI_FOOTBALL_MIN_AREA_PER_HUMANOID
 from dm_control.locomotion.soccer.pitch import Pitch
 from dm_control.locomotion.soccer.pitch import RandomizedPitch
 from dm_control.locomotion.soccer.soccer_ball import regulation_soccer_ball
@@ -37,11 +40,13 @@ from dm_control.locomotion.soccer.team import Player
 from dm_control.locomotion.soccer.team import RGBA_BLUE
 from dm_control.locomotion.soccer.team import RGBA_RED
 from dm_control.locomotion.soccer.team import Team
+import numpy as np
 
 
 class WalkerType(enum.Enum):
   BOXHEAD = 0
   ANT = 1
+  HUMANOID = 2
 
 
 def _make_walker(name, walker_id, marker_rgba, walker_type=WalkerType.BOXHEAD):
@@ -54,6 +59,12 @@ def _make_walker(name, walker_id, marker_rgba, walker_type=WalkerType.BOXHEAD):
     )
   if walker_type == WalkerType.ANT:
     return walkers.Ant(name=name, marker_rgba=marker_rgba)
+  if walker_type == WalkerType.HUMANOID:
+    return Humanoid(
+        name=name,
+        marker_rgba=marker_rgba,
+        walker_id=walker_id,
+        visual=Humanoid.Visual.JERSEY)
   raise ValueError("Unrecognized walker type: %s" % walker_type)
 
 
@@ -70,10 +81,18 @@ def _make_players(team_size, walker_type):
   return home_players + away_players
 
 
+def _area_to_size(area, aspect_ratio=0.75):
+  """Convert from area and aspect_ratio to (width, height)."""
+  return np.sqrt([area / aspect_ratio, area * aspect_ratio]) / 2.
+
+
 def load(team_size,
          time_limit=45.,
          random_state=None,
          disable_walker_contacts=False,
+         enable_field_box=False,
+         keep_aspect_ratio=False,
+         terminate_on_goal=True,
          walker_type=WalkerType.BOXHEAD):
   """Construct `team_size`-vs-`team_size` soccer environment.
 
@@ -84,6 +103,12 @@ def load(team_size,
     random_state: (optional) an int seed or `np.random.RandomState` instance.
     disable_walker_contacts: (optional) if `True`, disable physical contacts
       between walkers.
+    enable_field_box: (optional) if `True`, enable physical bounding box for
+      the soccer ball (but not the players).
+    keep_aspect_ratio: (optional) if `True`, maintain constant pitch aspect
+      ratio.
+    terminate_on_goal: (optional) if `False`, continuous game play across
+      scoring events.
     walker_type: the type of walker to instantiate in the environment.
 
   Returns:
@@ -93,12 +118,32 @@ def load(team_size,
     ValueError: If `team_size` is not between 1 and 11.
     ValueError: If `walker_type` is not recognized.
   """
+  goal_size = None
+  min_size = (32, 24)
+  max_size = (48, 36)
+  ball = SoccerBall()
+
+  if walker_type == WalkerType.HUMANOID:
+    goal_size = MINI_FOOTBALL_GOAL_SIZE
+    num_walkers = team_size * 2
+    min_size = _area_to_size(MINI_FOOTBALL_MIN_AREA_PER_HUMANOID * num_walkers)
+    max_size = _area_to_size(MINI_FOOTBALL_MAX_AREA_PER_HUMANOID * num_walkers)
+    ball = regulation_soccer_ball()
+
+  task_factory = Task
+  if not terminate_on_goal:
+    task_factory = MultiturnTask
 
   return composer.Environment(
-      task=Task(
+      task=task_factory(
           players=_make_players(team_size, walker_type),
           arena=RandomizedPitch(
-              min_size=(32, 24), max_size=(48, 36), keep_aspect_ratio=True),
+              min_size=min_size,
+              max_size=max_size,
+              keep_aspect_ratio=keep_aspect_ratio,
+              field_box=enable_field_box,
+              goal_size=goal_size),
+          ball=ball,
           disable_walker_contacts=disable_walker_contacts),
       time_limit=time_limit,
       random_state=random_state)
--- dm_control/locomotion/soccer/explore.py
+++ dm_control/locomotion/soccer/explore.py
@@ -17,15 +17,39 @@
 
 import functools
 from absl import app
+from absl import flags
 from dm_control import viewer
 from dm_control.locomotion import soccer
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_enum("walker_type", "BOXHEAD", ["BOXHEAD", "ANT", "HUMANOID"],
+                  "The type of walker to explore with.")
+flags.DEFINE_bool(
+    "enable_field_box", True,
+    "If `True`, enable physical bounding box enclosing the ball"
+    " (but not the players).")
+flags.DEFINE_bool("disable_walker_contacts", False,
+                  "If `True`, disable walker-walker contacts.")
+flags.DEFINE_bool(
+    "terminate_on_goal", False,
+    "If `True`, the episode terminates upon a goal being scored.")
 
 
 def main(argv):
   if len(argv) > 1:
-    raise app.UsageError('Too many command-line arguments.')
-  viewer.launch(environment_loader=functools.partial(soccer.load, team_size=2))
+    raise app.UsageError("Too many command-line arguments.")
+
+  viewer.launch(
+      environment_loader=functools.partial(
+          soccer.load,
+          team_size=2,
+          walker_type=soccer.WalkerType[FLAGS.walker_type],
+          disable_walker_contacts=FLAGS.disable_walker_contacts,
+          enable_field_box=FLAGS.enable_field_box,
+          keep_aspect_ratio=True,
+          terminate_on_goal=FLAGS.terminate_on_goal))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   app.run(main)
