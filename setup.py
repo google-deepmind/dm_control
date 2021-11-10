75939a0eca707e0e70cfb34370ca250a613d6e93
DeepMind <noreply@google.com>
2021-11-10 03:40:11 -0800

    Internal change
    PiperOrigin-RevId: 408836585
    Change-Id: I8fda2e30ab30d9f64343a6a00895616dca29118b

--- setup.py
+++ setup.py
@@ -175,7 +175,7 @@ def find_data_files(package_dir, patterns, excludes=()):
 
 setup(
     name='dm_control',
-    version='0.0.408319720',
+    version='0.0.408836585',
     description='Continuous control environments and MuJoCo Python bindings.',
     author='DeepMind',
     license='Apache License, Version 2.0',
