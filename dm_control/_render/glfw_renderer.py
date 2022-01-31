fe4449606742a7b8bec81930790b98244cddc538
Saran Tunyasuvunakool <stunya@google.com>
2022-01-31 05:25:08 -0800

    Revert commit 73c31ac21d1214f0a2eadfc8540a7f76796955cc.
    That commit broke rendering on macOS. `glfwInitHint` only sets internal hints in GLFW and a subsequent call to `glfwInit` is still required. However, `glfwInitHint(GLFW_COCOA_MENUBAR, 0); glfwInit();` still results in a dock icon being created, so we are reverting that commit entirely until we figure out a way to actually disable it.
    
    Closes #239
    
    PiperOrigin-RevId: 425341097
    Change-Id: If87643db372d4ec0a5237deb6c0b3effecdd7dc0

--- dm_control/_render/glfw_renderer.py
+++ dm_control/_render/glfw_renderer.py
@@ -15,8 +15,6 @@
 
 """An OpenGL renderer backed by GLFW."""
 
-import platform
-
 from dm_control._render import base
 from dm_control._render import executor
 
@@ -27,13 +25,7 @@ try:
 except (ImportError, IOError, OSError) as exc:
   raise ImportError from exc
 try:
-  if platform.system() == 'Darwin':
-    # By default, GLFW creates a Cocoa menubar on macOS, which results in a
-    # Python rocket icon appearing in the dock. We don't want this since
-    # dm_control._render only uses GLFW for offscreen rendering.
-    glfw.init_hint(glfw.COCOA_MENUBAR, glfw.FALSE)
-  else:
-    glfw.init()
+  glfw.init()
 except glfw.GLFWError as exc:
   raise ImportError from exc
 
