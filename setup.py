google-deepmind/dm_control

commit 2046be23ecfdb47755bf967f925600b96825e4e2

Remove stray `#@test` annotation from tutorial notebook
PiperOrigin-RevId: 425474013
Change-Id: I58098447c77c1f2f5f86b7a649450d334913e578

setup.py
@@ -192,7 +192,7 @@ def find_data_files(package_dir, patterns, excludes=()):
 
 setup(
     name='dm_control',
-    version='0.0.425341097',
+    version='0.0.425474013',
     description='Continuous control environments and MuJoCo Python bindings.',
     author='DeepMind',
     license='Apache License, Version 2.0',

tutorial.ipynb
@@ -1806,8 +1806,7 @@
       },
       "outputs": [],
       "source": [
-        "#@title Loading and simulating a `manipulation` task{vertical-output: true}\n",
-        "#@test {\"timeout\": 180}\n",
+        "#@title Loading and simulating a `manipulation` task{vertical-output: true}\n
         "\n",
         "env = manipulation.load('stack_2_of_3_bricks_random_order_vision', seed=42)\n",
         "action_spec = env.action_spec()\n",

commit 4110221701c2666df21953b55e98e5c552485599

Fix missing quotation mark in colab.
2046be23ecfdb47755bf967f925600b96825e4e2 accidentally removed the quotation mark and comma from the end of a line, breaking the notebook.

PiperOrigin-RevId: 429265583
Change-Id: I3453610a4a1d88798a915faeab0310f9522a8476

setup.py
@@ -192,7 +192,7 @@ def find_data_files(package_dir, patterns, excludes=()):
 
 setup(
     name='dm_control',
-    version='0.0.428777756',
+    version='0.0.429265583',
     description='Continuous control environments and MuJoCo Python bindings.',
     author='DeepMind',
     license='Apache License, Version 2.0',

tutorial.ipynb
@@ -1806,7 +1806,7 @@
       },
       "outputs": [],
       "source": [
-        "#@title Loading and simulating a `manipulation` task{vertical-output: true}\n
+        "#@title Loading and simulating a `manipulation` task{vertical-output: true}\n",
         "\n",
         "env = manipulation.load('stack_2_of_3_bricks_random_order_vision', seed=42)\n",
         "action_spec = env.action_spec()\n",