6bbaf99aabe5cc93dd0b3dcbde70f495cdfa6aaf
Nimrod Gileadi <nimrod@google.com>
2022-03-09 09:41:11 -0800

    internal change
    PiperOrigin-RevId: 433501881
    Change-Id: I9af20f6f33949c4faa0be360bc2043270b7245f1

--- setup.py
+++ setup.py
@@ -193,7 +193,7 @@ def find_data_files(package_dir, patterns, excludes=()):
 
 setup(
     name='dm_control',
-    version='0.0.433461696',
+    version='0.0.433501881',
     description='Continuous control environments and MuJoCo Python bindings.',
     author='DeepMind',
     license='Apache License, Version 2.0',
