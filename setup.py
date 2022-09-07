60e18a5ea1790f8de7f58f0002eaa6f73b617cdd
Saran Tunyasuvunakool <stunya@google.com>
2022-09-07 02:58:51 -0700

    Bump version number to 1.0.6.
    PiperOrigin-RevId: 472678116
    Change-Id: I754ae8817c41bebbaab41c5e439dfd1d6518a822

--- setup.py
+++ setup.py
@@ -173,7 +173,7 @@ def find_data_files(package_dir, patterns, excludes=()):
 
 setup(
     name='dm_control',
-    version='1.0.5',
+    version='1.0.6',
     description='Continuous control environments and MuJoCo Python bindings.',
     long_description="""
 # `dm_control`: DeepMind Infrastructure for Physics-Based Simulation.
@@ -201,7 +201,7 @@ notebook: [Open In Google Colab](https://colab.research.google.com/github/deepmi
         'glfw',
         'labmaze',
         'lxml',
-        'mujoco >= 2.2.1',
+        'mujoco >= 2.2.2',
         'numpy >= 1.9.0',
         'protobuf >= 3.19.4',  # TensorFlow requires protobuf<3.20 (b/182876485)
         'pyopengl >= 3.1.4',
