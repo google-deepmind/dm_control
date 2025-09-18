3831fa523c160fe653d83a829d0a0c59715f5725
DeepMind <noreply@google.com>
2025-09-18 11:31:35 -0700

    Bump dm_control version
    PiperOrigin-RevId: 808666717
    Change-Id: I6255821c44a596bc4281956184fd0d8445209c33

--- setup.py
+++ setup.py
@@ -166,7 +166,7 @@ def find_data_files(package_dir, patterns, excludes=()):
 
 setup(
     name='dm_control',
-    version='1.0.33',
+    version='1.0.34',
     description='Continuous control environments and MuJoCo Python bindings.',
     long_description="""
 # `dm_control`: DeepMind Infrastructure for Physics-Based Simulation.
@@ -191,7 +191,7 @@ notebook: [Open In Google Colab](https://colab.research.google.com/github/google
         'glfw',
         'labmaze',
         'lxml',
-        'mujoco >= 3.3.5',
+        'mujoco >= 3.3.6',
         'numpy >= 1.9.0',
         'protobuf >= 3.19.4',
         'pyopengl >= 3.1.4',
