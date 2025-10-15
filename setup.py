049656f5b19e67b18b074bdbe3a804bc1caac69f
Gabe Oppenheimer <goppenheimer@google.com>
2025-10-15 15:33:34 -0700

    Update dm_control following the MuJoCo 3.3.7 release.
    PiperOrigin-RevId: 819939614
    Change-Id: Ib713567d217560200a1200ae012a447359a26e91

--- setup.py
+++ setup.py
@@ -166,7 +166,7 @@ def find_data_files(package_dir, patterns, excludes=()):
 
 setup(
     name='dm_control',
-    version='1.0.34',
+    version='1.0.35',
     description='Continuous control environments and MuJoCo Python bindings.',
     long_description="""
 # `dm_control`: DeepMind Infrastructure for Physics-Based Simulation.
@@ -191,7 +191,7 @@ notebook: [Open In Google Colab](https://colab.research.google.com/github/google
         'glfw',
         'labmaze',
         'lxml',
-        'mujoco >= 3.3.6',
+        'mujoco >= 3.3.7',
         'numpy >= 1.9.0',
         'protobuf >= 3.19.4',
         'pyopengl >= 3.1.4',
