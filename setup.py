dace40506e5e987ad7db652d622469be6d93495c
DeepMind <noreply@google.com>
2026-04-14 07:18:24 -0700

    Update to version 1.0.39 following the MuJoCo 3.7.0 release.
    PiperOrigin-RevId: 899572533
    Change-Id: I6553def744314a789a761156174ca01fa162d00f

--- setup.py
+++ setup.py
@@ -166,7 +166,7 @@ def find_data_files(package_dir, patterns, excludes=()):
 
 setup(
     name='dm_control',
-    version='1.0.38',
+    version='1.0.39',
     description='Continuous control environments and MuJoCo Python bindings.',
     long_description="""
 # `dm_control`: DeepMind Infrastructure for Physics-Based Simulation.
@@ -191,7 +191,7 @@ notebook: [Open In Google Colab](https://colab.research.google.com/github/google
         'glfw',
         'labmaze',
         'lxml',
-        'mujoco >= 3.6.0',
+        'mujoco >= 3.7.0',
         'numpy >= 1.9.0',
         'protobuf >= 3.19.4',
         'pyopengl >= 3.1.4',
